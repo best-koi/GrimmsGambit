0b3d4345e0cfebfb6c52f7c50c79f42cdb462249
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2024-10-11 14:09:31 -0700

    Created Start of MapPlayer Script

---
+++ Grimm's Gambit/Assets/Scripts/MapScripts/MapPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject currentLocation;//A GameObject representing a position on the map, will be changed for eventual Encounter object 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
