bd1c645de470a27c32b06b802559f567a09c4602
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2024-10-25 14:27:08 -0700

    Created Abstract Class for Enemy
    -EnemyTemplate allows for other classes to inherit
    -Should help modify enemy behaviors

---
+++ Grimm's Gambit/Assets/Scripts/EnemyFamily/EnemyTemplate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyTemplate : MonoBehaviour
+{
+    [SerializeField]
+    protected int hp;
+
+    [SerializeField]
+    protected string enemyName;
+
+}
