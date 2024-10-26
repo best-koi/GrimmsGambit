c76016bc76461dcd1e044ae7542b24016728ccb0
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2024-10-26 11:15:55 -0700

    Added Enemy Subclasses
    -Also, made an AttackPattern() script for iterating through an enemy's available attacks

--- Grimm's Gambit/Assets/Scripts/EnemyFamily/EnemyTemplate.cs
+++ Grimm's Gambit/Assets/Scripts/EnemyFamily/EnemyTemplate.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Code by Jessie Archer
 public abstract class EnemyTemplate : MonoBehaviour
 {
     [SerializeField]
-    protected int hp;
+    protected int hp;//A hit points stat for the enemy
 
     [SerializeField]
-    protected string enemyName;
+    protected string enemyName;//The enemy's name
+
+    [SerializeField]
+    protected List<System.Action> attacks;//A list of methods for attacks
+
+    [SerializeField]
+    protected int currentAttack;//The index representing the current attack
+
+    //AttackPattern() essentially calls the next attack from the list
+    //Once the attack is done, it advances to the next attack in the pattern
+    //Also, checks for going out of bounds
+    protected virtual void AttackPattern()
+    {
+        if (currentAttack >= attacks.Count)
+            currentAttack = 0;
+
+        //Calls a method from the list of available attacks
+        attacks[currentAttack]();
+        //Moves onto the next attack
+        currentAttack++;
+
+        
+    }
 
 }
