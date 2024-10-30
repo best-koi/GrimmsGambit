92e76f0b5ad71266215b14abddc2f67d7d5c6323
CelestialMagic <116318917+CelestialMagic@users.noreply.github.com>
2024-10-30 10:15:27 -0700

    Made CharacterTemplate and BasicCharacter Classes

---
+++ Grimm's Gambit/Assets/Scripts/CharacterScripts/CharacterTemplate.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public abstract class CharacterTemplate : MonoBehaviour
+{ 
+[SerializeField]
+    protected int hp, maxHP;//Hit points stat 
+
+[SerializeField]
+protected string characterName;//The characters's name
+
+[SerializeField]
+protected TMP_Text healthText, nameText;//Text to indicate the character's health
+
+[SerializeField]
+protected Color characterColor;//A color that represents the character
+
+[SerializeField]
+protected Renderer renderer;//The character's renderer
+
+//A default Start() method 
+protected virtual void Start()
+{
+    //Sets color to preset color
+    renderer.material.color = characterColor;
+}
+
+    //Shows the default text above and below Character
+    protected virtual void Update()
+{
+    healthText.text = $"{hp}/ {maxHP}";
+    nameText.text = characterName;
+
+}
+
+//Sets the enemy hp
+//Put in positive number to increase
+//Put in negative number to decrease
+public void AffectHP(int amount)
+{
+    hp += amount;
+}
+
+//sets HP to a fixed amount
+public void SetHP(int amount)
+{
+    hp = amount;
+}
+
+//Returns the Character's Name
+public string GetCharacterName()
+{
+    return characterName;
+}
+
+ //Returns the Character's Color (for Intent)
+ public Color GetCharacterColor()
+{
+    return characterColor;
+}
+
+//Returns the Character's Current Health
+public int GetHP()
+{
+    return hp;
+}
+
+//Returns the Character's Max Health
+public int GetMaxHP()
+{
+    return maxHP;
+}
+}
