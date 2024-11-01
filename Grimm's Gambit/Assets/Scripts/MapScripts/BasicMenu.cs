3dfefd33142f7341fd5743288a800d10a0df5980
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2024-11-01 11:35:24 -0700

    Split Menu Into Two Demos

--- Grimm's Gambit/Assets/Scripts/MapScripts/BasicMenu.cs
+++ Grimm's Gambit/Assets/Scripts/MapScripts/BasicMenu.cs
@@ -9,9 +9,25 @@ public class BasicMenu : MonoBehaviour
     [SerializeField]
     private string startScene;
 
+    [SerializeField]
+    private string mapScene;
+
+    [SerializeField]
+    private string enemyScene;
+
     public void LoadStartScene()
     {
         SceneManager.LoadScene(startScene);
+    }
+
+    public void EnemyScene()
+    {
+        SceneManager.LoadScene(enemyScene);
+    }
+
+    public void MapScene()
+    {
+        SceneManager.LoadScene(mapScene);
     }
 
     public void QuitGame()
