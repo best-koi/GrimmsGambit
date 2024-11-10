95f4b8c9cabec4a8fd6691c65904c1d526eb0e9d
RyanLockie16 <RyanLockie16@gmail.com>
2024-11-09 20:18:08 -0800

    Implemented all heir cards except Focus and Gifted Tactics

--- Grimm's Gambit/Assets/Scripts/Card Scripts/Spell Components/RandomDebuff.cs
+++ Grimm's Gambit/Assets/Scripts/Card Scripts/Spell Components/RandomDebuff.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class RandomDebuff : SpellComponent
 {
-    public Affix[] debuffs;
-    public int[] values;
+    public int quantity;
     public RandomDebuff()
     {
         requiresTarget = true;
     }
 
-    public override void DoSpellEffect()
+    public override void DoSpellEffect() //Function to apply a random debuff effect to the target character
     {
-        // Unsure of what function is called to add affixes to Minions, here is filler code
-        //Updated Version Added by Ryan - 11/1/2024:
-        for(int i = 0; i < debuffs.Length; i++) 
+        System.Random random = new System.Random();
+        int debuffOption = random.Next(0, 6);
+        switch(debuffOption)
         {
-            target.AddAffix(debuffs[i], values[i]);
+            case 0:
+                target.AddAffix(Affix.DamageReduction, quantity);
+                break;
+            case 1:
+                target.AddAffix(Affix.Vulnerable, quantity);
+                break;
+            case 2:
+                target.AddAffix(Affix.Bleed, quantity);
+                break;
+            case 3:
+                target.AddAffix(Affix.Mark, quantity);
+                break;
+            case 4:
+                target.AddAffix(Affix.Threaded, quantity);
+                break;
+            case 5:
+                target.AddAffix(Affix.Exploit, quantity);
+                break;
+            default: //This should never occur
+                break;
         }
     }
 }
