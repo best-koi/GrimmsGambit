8fea51fd5789baf438822ab41744a312f5e86c7c
RyanLockie16 <RyanLockie16@gmail.com>
2024-11-10 11:18:40 -0800

    Fixed Swap Spell

--- Grimm's Gambit/Assets/Scripts/Card Scripts/Spell Components/Swap.cs
+++ Grimm's Gambit/Assets/Scripts/Card Scripts/Spell Components/Swap.cs
@@ -9,8 +9,38 @@ public class Swap : SpellComponent
         spellName = "Swap";
         spellDescription = "When this card is played, position is swapped with the target character.";
     }
-    public override void DoSpellEffect()
+    public override void DoSpellEffect() //Swaps positions of caster and target
     {
-        //Insert function from unit party here to swap position with between "caster" and "target"
+        //Retrieves unit friendly unit party reference
+        List<GameObject> friendlyParty;
+        UnitParty friendlyUnitParty;
+        UnitParty[] parties = FindObjectsOfType<UnitParty>();
+        List<GameObject> currentParty = parties[0].GetAllMembers();   
+        if (currentParty[0].GetComponent<Minion>().ownerPlayer == true)
+        {
+            friendlyParty = currentParty;
+            friendlyUnitParty = parties[0];
+        }
+        else
+        {
+            friendlyParty = parties[1].GetAllMembers();
+            friendlyUnitParty = parties[1];
+        }
+        //Determines indexes within the unit party for caster and target minions
+        int casterIndex = 0;
+        int targetIndex = 0;
+        for(int i = 0; i < friendlyParty.Count; i++)
+        {
+            if (target == friendlyParty[i].GetComponent<Minion>()) //Checks if target is current minion
+            {
+                targetIndex = i;
+            }
+            else if (caster == friendlyParty[i].GetComponent<Minion>()) //Checks if caster is current minion
+            {
+                casterIndex = i;
+            }
+        }
+        //Performs swap with determined index values
+        friendlyUnitParty.SwitchMemberIndices(targetIndex, casterIndex);
     }
 }
