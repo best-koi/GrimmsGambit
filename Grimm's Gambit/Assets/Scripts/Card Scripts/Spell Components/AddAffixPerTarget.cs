8d755e4982154ba7c1be6185a05f52f2a29fbd0d
cbb1997 <cbd45068@gmail.com>
2024-11-12 20:39:58 -0800

    Finished hound cards and added some merc.

---
+++ Grimm's Gambit/Assets/Scripts/Card Scripts/Spell Components/AddAffixPerTarget.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddAffixPerTarget : SpellComponent
+{
+    [SerializeField] private Affix[] debuffs;
+    [SerializeField] private int[] values;
+
+    // True to apply based on if the enemies are targetting
+    // False if to check for the enemies that have yet to target
+    [SerializeField] private bool m_IsTargetting, m_CheckingSelf;
+
+    public AddAffixPerTarget()
+    {
+        spellName = "Add Affix Per Target";
+        spellDescription = "Add an affix based on the number of enemies targetting (or not targetting) this enemy.";
+
+        if (!m_CheckingSelf) requiresTarget = true;
+    }
+    public override void DoSpellEffect()
+    {
+        //The following assumes that only two parties exist and characters have properly labeled owners.
+        //This is used to ensure that the friendly party is healed
+        List<GameObject> enemyParty;
+        UnitParty[] parties = FindObjectsOfType<UnitParty>();
+        List<GameObject> currentParty = parties[0].GetAllMembers();
+        if (!currentParty[0].GetComponent<Minion>().ownerPlayer)
+        {
+            enemyParty = currentParty;
+        }
+        else
+        {
+            enemyParty = parties[1].GetAllMembers();
+        }
+
+        Minion minionToCheck;
+        if (requiresTarget) minionToCheck = caster;
+        else minionToCheck = target;
+
+        foreach (GameObject member in enemyParty)
+        {
+            if (member.GetComponent<EnemyTemplate>().GetAttackTarget() == minionToCheck && m_IsTargetting) AddAffix(minionToCheck);
+            else if (member.GetComponent<EnemyTemplate>().GetAttackTarget() != minionToCheck && !m_IsTargetting) AddAffix(minionToCheck);
+        }
+    }
+
+    // Helper method
+    private void AddAffix(Minion affixTarget)
+    {
+        for (int i = 0; i < debuffs.Length; i++)
+        {
+            affixTarget.AddAffix(debuffs[i], values[i]);
+        }
+    }
+}
+
