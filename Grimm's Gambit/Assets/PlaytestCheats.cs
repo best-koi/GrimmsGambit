11eb05f03463a4b03c7289a5db25164ead13e69c
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2024-12-10 21:25:27 -0800

    Revert "Edited Card Fonts"
    This reverts commit d15922448327a18992f191795387935729d00fab.

--- Grimm's Gambit/Assets/PlaytestCheats.cs
+++ Grimm's Gambit/Assets/PlaytestCheats.cs
@@ -1,33 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using TMPro;
+using UnityEngine.SceneManagement; 
+
 public class PlaytestCheats : MonoBehaviour
 {
-    [SerializeField]
-    private TMP_FontAsset font;
-
-    private static TMP_FontAsset allFont;
-
     // Start is called before the first frame update
     void Start()
     {
-        allFont = font;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
         if(Input.GetKeyDown("1")){
             SceneManager.LoadScene("EnemyCombatScene");
         }
-        */
-    }
-
-    public static TMP_FontAsset GetAllFont()
-    {
-        return allFont; 
     }
 }
