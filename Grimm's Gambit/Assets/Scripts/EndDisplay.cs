935c684c227563e7b9349221dead56737c221a4a
cbb1997 <cbd45068@gmail.com>
2025-01-22 13:12:29 -0800

    Fixed end display and made cards remove on character death

--- Grimm's Gambit/Assets/Scripts/EndDisplay.cs
+++ Grimm's Gambit/Assets/Scripts/EndDisplay.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class EndDisplay : MonoBehaviour
 {
+    [SerializeField] private GameObject endCamera;
+
     [SerializeField] private string winText = "You defeated the enemy", loseText = "You were slain...";
     [SerializeField] private TMP_Text endScreenText;
 
     public void SetDisplay(bool win)
     {
-        Debug.Log("Called");
         if (win) endScreenText.text = winText;
         else endScreenText.text = loseText;
+    }
+
+    private void OnEnable()
+    {
+        endCamera.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        endCamera.SetActive(false);
     }
 }
