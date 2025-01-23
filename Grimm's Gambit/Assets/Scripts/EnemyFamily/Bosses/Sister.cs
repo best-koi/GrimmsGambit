eeed563841e89b0d2ae8565ad867863ea7e3166e
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-01-22 21:43:10 -0800

    Added Comments on Sister Script

--- Grimm's Gambit/Assets/Scripts/EnemyFamily/Bosses/Sister.cs
+++ Grimm's Gambit/Assets/Scripts/EnemyFamily/Bosses/Sister.cs
@@ -6,9 +6,11 @@ public class Sister : Lycan
 {
     protected static int randomStartAttack;//A static variable that should match for both sisters 
     
+/*The Sister Script will function similar to the Lycan with some adjustments
+A static variable will be used to ensure both sisters start on the same attack number
+*/
 
-
-    // Start is called before the first frame update
+    // Start here is used to sync up the sister attack patterns
     protected override void Start()
     {
         randomStartAttack = Random.Range(0, attacks.Count);
