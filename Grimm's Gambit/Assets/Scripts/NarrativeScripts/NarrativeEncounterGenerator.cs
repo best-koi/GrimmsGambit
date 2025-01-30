13e051ef6d9464d1200136128a1c035c58196c37
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-01-29 18:28:35 -0800

    Created Encounter Generator Script

--- Grimm's Gambit/Assets/Scripts/NarrativeScripts/NarrativeEncounterGenerator.cs
+++ Grimm's Gambit/Assets/Scripts/NarrativeScripts/NarrativeEncounterGenerator.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NarrativeEncounterGenerator : MonoBehaviour
 {
     [SerializeField]
-    private List<NarrativeEncounter> narrativeEncounters; 
+    private List<NarrativeEncounter> narrativeEncounters; //A list of all narrative encounters to pull from
+
+    [SerializeField]
+    private GameObject twoChoiceUI;
+
+    [Header("General UI Elements")]
+    [SerializeField]
+    private TMP_Text twoTitle, twoDesc, twoChoice1, twoChoice2, twoChoiceOutcome1, twoChoiceOutcome2;
+
+
+    [SerializeField]
+    private GameObject threeChoiceUI;
+    
+    [Header("Three Choice UI Elements")]
+    [SerializeField]
+    private TMP_Text threeChoiceTitle, threeChoiceDesc, threeChoice1, threeChoice2, threeChoice3, threeChoiceOutcome1, threeChoiceOutcome2, threeChoiceOutcome3;
+
+
+
+
 
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
