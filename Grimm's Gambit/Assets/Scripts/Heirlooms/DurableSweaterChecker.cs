116977a653fbd30caf087155981fb6f7296369b6
RyanLockie16 <RyanLockie16@gmail.com>
2025-02-02 01:50:06 -0800

    All Heirlooms implemented, visual status display is now needed.

--- Grimm's Gambit/Assets/Scripts/Heirlooms/DurableSweaterChecker.cs
+++ Grimm's Gambit/Assets/Scripts/Heirlooms/DurableSweaterChecker.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class DurableSweaterChecker : MonoBehaviour
 {
+    public Minion seamtressMinion;
+    HeirloomManager heirloomManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        EncounterController.onTurnChanged += TurnUpdate; //Connects turn update function to the turn transitions
+        heirloomManager = FindObjectOfType<HeirloomManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TurnUpdate(bool currentPlayer)
     {
-        
+        if (!currentPlayer && heirloomManager.ContainsHeirloom(Heirloom.Sweater)) //Player turn end when sweater is equipped
+        {
+            seamtressMinion.AddAffix(Affix.Block, 4);
+        }
     }
 }
--- Grimm's Gambit/Assets/Scripts/Heirlooms/SerpentRingChecker.cs
+++ Grimm's Gambit/Assets/Scripts/Heirlooms/SerpentRingChecker.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SerpentRingChecker : MonoBehaviour
 {
+    public Minion houndMinion;
+    private bool RingActive = false; //Determines whether the serpent rings' strength proc is activated
+    HeirloomManager heirloomManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        EncounterController.onTurnChanged += TurnUpdate; //Connects turn update function to the turn transitions
+        heirloomManager = FindObjectOfType<HeirloomManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TurnUpdate(bool currentPlayer)
     {
-        
+        if (currentPlayer) //Player turn start - Block stacks are reset here
+        {
+            if (RingActive)
+            {
+                houndMinion.AddAffix(Affix.Strength, 1); //Adds 1 Strength at the start of each turn, while the ring is active
+            }
+        }
+        else //Enemy turn (Player turn end)
+        {
+            if (houndMinion.currentAffixes.ContainsKey(Affix.Block))
+            {
+                if (heirloomManager.ContainsHeirloom(Heirloom.Serpent) && houndMinion.currentAffixes[Affix.Block] >= 12)
+                {
+                    RingActive = true; //The ring becomes activated for the duration of a combat once block is greater than 12 at the end of any player turn on the hound.
+                }
+            }
+        }
     }
 }
