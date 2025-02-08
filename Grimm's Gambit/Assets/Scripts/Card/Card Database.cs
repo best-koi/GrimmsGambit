920cb64be765c163adeb95981ae45dea2af5ef4e
RyanLockie16 <RyanLockie16@gmail.com>
2025-02-08 14:34:56 -0800

    Implented Victory Drafting Scene
    Needs to be attached to the save system before it can be implemented properly to the overall game.

--- Grimm's Gambit/Assets/Scripts/Card/Card Database.cs
+++ Grimm's Gambit/Assets/Scripts/Card/Card Database.cs
@@ -5,7 +5,7 @@ public class CardDatabase : MonoBehaviour
 {
     [Tooltip("The card prefab used to spawn card objects from template.")] [SerializeField] private GameObject _cardPrefab;
     [Tooltip(" Array of party members. Can be changed to accodomadate Unit Party or other data types.")] [SerializeField] private Minion[] _partyIndex;
-    [Tooltip("New databse representing cards.")] [SerializeField] private Array2D _cardPrefabs;
+    [Tooltip("New databse representing cards.")] [SerializeField] public Array2D _cardPrefabs; //Made public by Ryan 2/8/2025 for shop menu purposes
 
     private void Start()
     {
