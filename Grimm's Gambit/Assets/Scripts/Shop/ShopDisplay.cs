d80215ea0b7f5fcb2c6e084e535392566411e218
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-02-07 16:12:21 -0800

    Removed Unused Currency Lines

--- Grimm's Gambit/Assets/Scripts/Shop/ShopDisplay.cs
+++ Grimm's Gambit/Assets/Scripts/Shop/ShopDisplay.cs
@@ -6,11 +6,6 @@ using TMPro;
 
 public class ShopDisplay : MonoBehaviour
 {
-    [SerializeField]
-    private TMP_Text eyeText, hairText, tongueText; //Text to display the loot drops 
-
-    [SerializeField]
-    private int eyeCount, hairCount, tongueCount;//Ints to test out shop values - to be taken from player loot script eventually 
 
    [SerializeField]
    private List<ShopItem> commonCards, heirlooms, arcana;//The items in the shop
@@ -42,9 +37,7 @@ public class ShopDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        eyeText.text = $"{eyeCount}";
-        hairText.text = $"{hairCount}";
-        tongueText.text = $"{tongueCount}";
+    
 
         //DisplayAllCycledItems(); 
 
