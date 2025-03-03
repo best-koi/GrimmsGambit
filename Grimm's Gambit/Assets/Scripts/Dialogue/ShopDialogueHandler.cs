c3c331be71abdbdabb30b521a4b6785968b8f855
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-03-02 20:03:49 -0800

    Added Dialogue Support for Shop
    - To be used for teaching players about each item in Shop

--- Grimm's Gambit/Assets/Scripts/Dialogue/ShopDialogueHandler.cs
+++ Grimm's Gambit/Assets/Scripts/Dialogue/ShopDialogueHandler.cs
@@ -15,10 +15,14 @@ public class ShopDialogueHandler : BossDialogueHandler
 //StartShopDialogue() is intended to display the shop dialogue for players
 //It is tied to a button click
 //This will likely just be information from Beatrice to the player 
-    protected void StartShopDialogue(){
+    public void StartShopDialogue(){
         index = 0;
         bossIndex = 0; 
+        conversationText.text = string.Empty; 
+        dialogueWindow.SetActive(true);
+        advanceButton.gameObject.SetActive(true);
         StartDialogue();
+        
 
     }
 }
