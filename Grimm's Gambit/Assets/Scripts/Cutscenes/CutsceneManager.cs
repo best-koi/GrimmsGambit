effe66ac966ed049431f4fb3e8e48a96952cda48
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-03-09 19:02:39 -0700

    Cutscene All Set Up

--- Grimm's Gambit/Assets/Scripts/Cutscenes/CutsceneManager.cs
+++ Grimm's Gambit/Assets/Scripts/Cutscenes/CutsceneManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CutsceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private VideoPlayer videoPlayer;//The cutscene VideoPlayer component
+
+    [SerializeField]
+    private GameObject cutsceneUI;//The UI object that contains the cutscene and background panel
+
+//Subscribes to an end event
+    private void Start(){
+        videoPlayer.loopPointReached += OnLoopPointReached; 
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+//Used to advance cutscene
+    public void SkipCutscene(){
+        cutsceneUI.SetActive(false);
+    }
+
+//Hides UI when cutscene is over
+    void OnLoopPointReached(VideoPlayer vp)
     {
-        
+        // Play the particle effect when the video reaches the end.  
+        SkipCutscene();
     }
 }
